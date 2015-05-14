5301fd347d1491dfb4b45b9700018be45ff766d9
dogura <daiki.ogura@soliton.co.jp>
2015-05-14 16:43:56 +0900

    デプロイタスクをgoとjsで分離

--- fabfile.py
+++ fabfile.py
@@ -15,20 +15,38 @@ def prepare():
         run('mkdir -p tiny-akasha/template/static/')
 
 
-def build():
+def build_server():
     local('GOOS=linux GOARCH=amd64 go build main.go')
+
+
+def build_assets():
     with lcd('./template/static/'):
         local('gulp build')
 
 
-def deploy():
-    prepare()
-    build()
+def build():
+    build_server()
+    build_assets()
+
+
+def deploy_server():
+    build_server()
     put('./main', '~/var/http/tiny-akasha/')
     put('./template/index.html', '~/var/http/tiny-akasha/template/index.html')
+
+
+def deploy_assets():
+    build_assets()
     put('./template/static/dist/', '~/var/http/tiny-akasha/template/static/')
     put('./template/static/node_modules/react/dist/react-with-addons.js', '~/var/http/tiny-akasha/template/static/dist/')
     put('./template/static/node_modules/react/dist/react-with-addons.min.js', '~/var/http/tiny-akasha/template/static/dist/')
+
+
+
+def deploy():
+    prepare()
+    deploy_server()
+    deploy_assets()
 
 
 def clean():
