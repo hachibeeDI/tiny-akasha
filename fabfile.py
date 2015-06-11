72e9e890e3e744b12a7b11cb160029dd7e68493d
OGURA_Daiki <8hachibee125@gmail.com>
2015-06-11 13:00:16 +0900

    毎回サードパーティをデプロイしないように

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,7 @@ env.use_ssh_config = True
 
 def prepare():
     with cd('~/var/http'):
-        run('mkdir -p tiny-akasha/template/static/')
+        run('mkdir -p tiny-akasha/template/static/dist/')
     put('./env/supervisord/goji.conf', '/etc/supervisor/conf.d/', use_sudo=True)
 
 
@@ -36,11 +36,23 @@ def deploy_server():
     put('./template/index.html', '~/var/http/tiny-akasha/template/index.html')
 
 
+def deploy_vendors():
+    ''' サードパーティのデプロイ '''
+    root = '~/var/http/tiny-akasha/template/static/'
+    run('mkdir -p {0}vendor/{{react,font/octicons}}'.format(root))
+    path_to_react = '{0}vendor/react/'.format(root)
+    if not exists('{0}react-with-addons.js'.format(path_to_react)):
+        put('./template/static/node_modules/react/dist/*.js', path_to_react)
+
+    path_to_octicon = '{0}vendor/font/octicons/'.format(root)
+    if not exists(path_to_octicon + 'octicons.ttf'):
+        put('./template/static/node_modules/octicons/octicons/*', path_to_octicon)
+
+
 def deploy_assets():
     build_assets()
+    deploy_vendors()
     put('./template/static/dist/', '~/var/http/tiny-akasha/template/static/')
-    put('./template/static/node_modules/react/dist/react-with-addons.js', '~/var/http/tiny-akasha/template/static/dist/')
-    put('./template/static/node_modules/react/dist/react-with-addons.min.js', '~/var/http/tiny-akasha/template/static/dist/')
 
 
 def deploy():
